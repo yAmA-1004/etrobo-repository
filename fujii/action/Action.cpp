75f23bba88c4fe0de01a8f236bac909af8a3cdca
Sugimoto Hiromu <134465359+yAmA-1004@users.noreply.github.com>
2024-05-14 13:34:14 +0900

    Delete fujii directory

--- fujii/action/Action.cpp
+++
@@ -1,9 +0,0 @@
-#ifndef ACTION_H_
-#define ACTION_H_
-
-class Action {
-public:
-    virtual void run() = 0;
-};
-
-#endif
--- fujii/action/ArmAction.cpp
+++
@@ -1,8 +0,0 @@
-#include "ArmAction.h"
-
-ArmAction::ArmAction(int armPWM) : armPWM_(armPWM) {
-}
-
-void ArmAction::run() {
-  Device::up(armPWM_);
-}
