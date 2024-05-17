9e18db56cfe4be2e273bf24e6ae4f4e8f8bda2ec
Sugimoto Hiromu <134465359+yAmA-1004@users.noreply.github.com>
2024-05-17 10:36:37 +0900

    Add files via upload

--- thy/strategy/ProcessSetup.cpp
+++ thy/strategy/ProcessSetup.cpp
@@ -17,7 +17,23 @@
 void Process::setup() {
   // 動作
  	Action* line_trace_right_action = new LineTraceAction(63, 63, 1, 0.0966, 0.01, 0.0387);//直線GPT これつかえばよいのでは
-  Action* line_trace_curve_action = new LineTraceAction(25, 25, 1, 0.15, 0.75, 0.015);
+  Action* line_trace_right_action2 = new LineTraceAction(25, 25, 1, 0.15, 0.75, 0.015);
+
+	Action* line_trace_left_action = new LineTraceAction(63, 63, -1, 0.0966, 0.01, 0.0387);
+	Action* line_trace_left_action2 = new LineTraceAction(25, 25, -1, 0.15, 0.75, 0.015);
+	Action* line_trace_rightcurve_action = new LineTraceAction(50, 50, 1, 0.45, 0.01, 0.055);//右ライントレースカーブgptゴールまでだけ
+
+	Action* line_trace_superslowsmallloop_action = new LineTraceAction(15, 15, -1, 0.15, 0.75, 0.0015);//小円　右
+	Action* straight_pwm20_action = new WheelAction(20, 20);
+	Action* straight_pwm40_action = new WheelAction(40, 40);
+	Action* right_turn_action = new WheelAction(5, -5);
+	Action* left_turn_action = new WheelAction(-5, 5);
+	Action* kirikae_action = new WheelAction(20, 16);
+	Action* kirikae_action2 = new WheelAction(4, 20);
+	Action* stop_action = new WheelAction(0, 0);
+	// Action* arm_up_action = new ArmAction(10);
+	// Action* arm_down_action = new ArmAction(-10);
+  
 
   // 監視
   Monitor* black_line_monitor = new BlackLineMonitor();
@@ -26,8 +42,8 @@ void Process::setup() {
 
   // シーン
   Scene* stop_scene = new Scene(stop_action, nullptr, nullptr);
-	// Scene* curve_scene = new Scene(line_trace_curve_action,new DistanceMonitor(2500),stop_scene)
-  Scene* start_scene = new Scene(line_trace_right_action, new DistanceMonitor(2000), stop_scene);
+	// Scene* curve_scene = new Scene(,new DistanceMonitor(),stop_scene)
+  Scene* start_scene = new Scene(line_trace_right_action, new DistanceMonitor(2000), stop_scene);//直線//uchi
   // 開始シーン
   // scene_ = block_entry_scene;
   // ブロックdeトレジャーから始める場合
