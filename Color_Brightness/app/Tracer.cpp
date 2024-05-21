28c1896b109ffa85974e1ce3c147c005cf77118a
Sugimoto Hiromu <134465359+yAmA-1004@users.noreply.github.com>
2024-05-21 13:12:30 +0900

    Add files via upload

--- Color_Brightness/app/Tracer.cpp
+++ Color_Brightness/app/Tracer.cpp
@@ -1,12 +1,12 @@
 #include "Tracer.h" // <1>
 #include "ColorSensor.h"
 
-static FILE *bt = NULL;
+// static FILE *bt = NULL;
 rgb_raw_t rgb;
 
 Tracer::Tracer():
   leftWheel(PORT_C), rightWheel(PORT_B), colorSensor(PORT_2) { // <2>
-    bt = ev3_serial_open_file(EV3_SERIAL_BT);
+    // bt = ev3_serial_open_file(EV3_SERIAL_BT);
 }
 
 void Tracer::init() {
@@ -18,10 +18,12 @@ void Tracer::terminate() {
 }
 
 void Tracer::run() {
-   colorSensor.getRawColor(rgb);
-   fprintf(bt, "R:%d G:%d B:%d\r\n", rgb.r, rgb.g, rgb.b);
+  colorSensor.getRawColor(rgb);
+  // fprintf(bt, "R:%d G:%d B:%d\r\n", rgb.r, rgb.g, rgb.b);
   //  fprintf(bt, "%d\r\n",colorSensor.getBrightness());
   //  fprintf(bt, "%d\r\n",colorSensor.getColorNumber());
+  sprintf(str, "R:%d G:%d B:%d", rgb.r, rgb.g, rgb.b);
+  hub_display_text_scroll(str, 100);
 
   msg_f("running...", 1);
 }
