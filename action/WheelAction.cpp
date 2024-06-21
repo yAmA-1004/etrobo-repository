c1382f8891085ab6d95eb2a3d469fcf5bc544d74
Sugimoto Hiromu <134465359+yAmA-1004@users.noreply.github.com>
2024-06-21 13:44:36 +0900

    Delete action directory

--- action/WheelAction.cpp
+++
@@ -1,8 +0,0 @@
-#include "WheelAction.h"
-
-WheelAction::WheelAction(int leftPWM, int rightPWM) : leftPWM_(leftPWM), rightPWM_(rightPWM) {
-}
-
-void WheelAction::run() {
-  Device::move(leftPWM_, rightPWM_);
-}
