81b640f2507b62c70bf832729189bca59aace795
YUKIHACHI <shibata.shoya11@gmail.com>
2024-06-24 11:25:34 +0900

    thyをgitignoreに

--- thy/monitor/RedBlockMonitor.h
+++
@@ -1,9 +0,0 @@
-#include "Monitor.cpp"
-#include "Device.h"
-#include "Env.h"
-
-class RedBlockMonitor : public Monitor {
-public:
-    RedBlockMonitor();
-    bool check();
-};
--- thy/monitor/RedLineMonitor.h
+++
@@ -1,8 +0,0 @@
-#include "Monitor.cpp"
-#include "Device.h"
-
-class RedLineMonitor : public Monitor {
-public:
-    RedLineMonitor();
-    bool check();
-};
