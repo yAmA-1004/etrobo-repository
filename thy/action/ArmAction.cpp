6e13bb83ed2f08f34729a49550ef5b8413c143a2
Sugimoto Hiromu <134465359+yAmA-1004@users.noreply.github.com>
2024-08-21 12:11:36 +0900

    Delete thy directory

--- thy/action/ArmAction.cpp
+++
@@ -1,9 +0,0 @@
-#include "ArmAction.h"
-
-ArmAction::ArmAction(int armPWM) : armPWM_(armPWM) {
-  //printf("armac:%d============ \n", armPWM);
-}
-
-void ArmAction::run() {
-  Device::up(armPWM_);
-}
